f0050bb6e51019debe18f43ec6be5b6bfec9a75f
Jessica Été <kohrVid@tutanota.com>
2024-03-31 14:58:07 +0200

    Create the User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_31_125120) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_31_125536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "pages", force: :cascade do |t|
     t.string "name"
     t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "registration_ip_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
