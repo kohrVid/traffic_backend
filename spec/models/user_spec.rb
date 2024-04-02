6d5a48afc620ac62902bda2b7b1ab8778430116f
Jessica Été <kohrVid@tutanota.com>
2024-04-02 14:15:40 +0200

    Ensure User's usernames are present and unique

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { create(:user) }
+
+  describe '#username' do
+    context 'when it is missing or blank' do
+      let(:new_user) do
+        User.new(username: '')
+      end
+
+      before { new_user.save }
+
+      it 'is invalid' do
+        expect(new_user).not_to be_valid
+      end
+
+      it 'raises the correct error message' do
+        expect(
+          new_user.errors.messages[:username]
+        ).to include("can't be blank")
+      end
+    end
+
+    context 'when a record with the same username already exists' do
+      let(:new_user) do
+        User.new(username: subject.username)
+      end
+
+      before do
+        subject
+        new_user.save
+      end
+
+      it 'is invalid' do
+        expect(new_user).not_to be_valid
+      end
+
+      it 'raises the correct error message' do
+        expect(
+          new_user.errors.messages[:username]
+        ).to include("has already been taken")
+      end
+    end
+  end
 end
