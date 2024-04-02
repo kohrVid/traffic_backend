7e8634cbbdcd89a690ca18d5356e93f78d529395
Jessica Été <kohrVid@tutanota.com>
2024-04-02 14:25:15 +0200

    WIP

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -2,4 +2,8 @@ class Visit < ApplicationRecord
   belongs_to :page
   belongs_to :user
   belongs_to :ip_info
+
+  scope :with_info, -> {
+    includes(:ip_info)
+  }
 end
--- spec/models/visit_spec.rb
+++ spec/models/visit_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Visit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    create(:visit)
+  end
+
+  describe '.with_info' do
+    it 'includes location data for each visit' do
+      debugger
+    end
+  end
 end
