6ec03fba39dfcccdd8a84ef152ea34726eb0bc91
Jessica Été <kohrVid@tutanota.com>
2024-04-03 21:49:47 +0200

    Add Page and IpInfo records to the seeds file
    Adding loopback IpInfo records could prevent some unnecessary API
    requests. This task also adds records for the three pages that have been
    set up to collect user's IP addresses in the frontend application.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+IpInfo.find_or_create_by(address: '::1', latitude: 0, longitude: 0, is_vpn: false)
+IpInfo.find_or_create_by(address: '127.0.0.1', latitude: 0, longitude: 0, is_vpn: false)
+Page.find_or_create_by(id: 1, name: 'index', url: 'http://localhost:1234/pages/')
+Page.find_or_create_by(id: 2, name: 'about', url: 'http://localhost:1234/pages/about')
+Page.find_or_create_by(id: 3, name: 'contact', url: 'http://localhost:1234/pages/contact')
