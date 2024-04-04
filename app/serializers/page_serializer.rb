fb359aa8fd034565cb05bc57bb8cd82884ef5459
Jessica Été <kohrVid@tutanota.com>
2024-04-04 03:01:51 +0200

    Add /pages index and serialiser
    This commit adds the Pages controller and its `#index` action, the
    response for which will be used in the dropdown list on the frontend.
    This also adds the PageSerializer which is used to format the page
    objects in the response body. The way in which rails handles
    serialisation seems to have changed significantly over the years. I have
    decided to create a serialiser that exists outside of the Page model
    because I believe that this maintains a separation of concerns.

---
+++ app/serializers/page_serializer.rb
@@ -0,0 +1,29 @@
+class PageSerializer
+  include ActiveModel::Serialization
+
+  def initialize(page = {})
+    @page = page
+  end
+
+  def attributes
+    {
+      id: nil,
+      name: nil,
+      url: nil
+    }
+  end
+
+  private
+
+  def id
+    @page[:id]
+  end
+
+  def name
+    @page[:name]
+  end
+
+  def url
+    @page[:url]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :pages, only: [:index]
   resources :visits, only: [:create]
 end
