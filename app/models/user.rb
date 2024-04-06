668427cd7adaa4e1332e65334f1c044711681255
Jessica Été <kohrVid@tutanota.com>
2024-04-06 04:52:21 +0200

    Accept nested attributes for `User#registration_ip_info`
    In order to store the user's first IP address, `User.create` must accept
    the IP address as a nested attribute. This commit adds this alongside a
    `#find_or_create_ip_info` before_validation (similar to the Visit
    model).
    I have also refactored some of the related unit tests as some of the
    older tests weren't using a factory already which caused issues where
    the IpInfo association was missing or duplicate.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,17 @@ class User < ApplicationRecord
 
   belongs_to :registration_ip_info, class_name: 'IpInfo'
 
+  accepts_nested_attributes_for :registration_ip_info
+
   validates :username, presence: true, uniqueness: { case_sensitive: false }
+
+  before_validation :find_or_create_ip_info
+
+  private
+
+  def find_or_create_ip_info
+    self.registration_ip_info = IpInfo.find_or_create_by(
+      address: registration_ip_info.address
+    )
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:ip_location) { double(:ip_location) }
+
+  before do
+    allow(IpLocation).to receive(:new)
+      .and_return(ip_location)
+
+    allow(ip_location).to receive(:coordinates)
+      .and_return([])
+
+    allow(ip_location).to receive(:vpn?).and_return(false)
+  end
+
   describe 'validations' do
     describe ':username' do
       context 'when it is missing or blank' do
         let(:new_user) do
-          User.new(username: '')
+          build(:user, username: '')
         end
 
         before { new_user.save }
@@ -22,9 +34,15 @@ RSpec.describe User, type: :model do
       end
 
       context 'when a record with the same username already exists' do
-        let(:existing_user) { create(:user) }
+        let(:ip_info) { create(:ip_info) }
+        let(:existing_user) { create(:user, registration_ip_info: ip_info) }
+
         let(:new_user) do
-          User.new(username: existing_user.username)
+          build(
+            :user,
+            registration_ip_info: ip_info,
+            username: existing_user.username
+          )
         end
 
         before do
@@ -41,6 +59,39 @@ RSpec.describe User, type: :model do
             new_user.errors.messages[:username]
           ).to include("has already been taken")
         end
+      end
+    end
+  end
+
+  describe '#save' do
+    subject do
+      build(:user, registration_ip_info_attributes: { address: ip_address })
+    end
+
+    let(:ip_info) { create(:ip_info) }
+    let(:ip_address) { ip_info.address }
+
+    before do
+      ip_info
+      subject.save
+    end
+
+    it 'associates the User with a registration IP' do
+      expect(subject.registration_ip_info).to be_a(IpInfo)
+    end
+
+    context 'when the IP address already exists in the database' do
+      it 'associates the User with an existing IpInfo record' do
+        expect(subject.registration_ip_info).to eq(ip_info)
+      end
+    end
+
+    context 'when the IP address does not exist in the database' do
+      let(:ip_address) { '213.152.176.139' }
+
+      it 'associates the User with a new IpInfo record' do
+        expect(subject.registration_ip_info).to eq(IpInfo.last)
+        expect(subject.registration_ip_info.address).to eq(ip_address)
       end
     end
   end
