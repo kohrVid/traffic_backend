f8ce4d0aa9c71b86e2645da5fd2bc06063fe7332
Jessica Été <kohrVid@tutanota.com>
2024-04-07 17:46:07 +0200

    Convert existing request specs to swagger docs
    Request and controller specs are considered somewhat controversial as
    they often end up testing the rails framework rather than the
    application code. This is even truer when used with API-mode
    applications as there is no frontend for the request spec to interact
    with. As this application already requires API documentation, I have
    decided to convert the existing request specs into more useful swagger
    documentation.

--- spec/requests/users/registrations_spec.rb
+++ spec/requests/users/registrations_spec.rb
@@ -1,57 +1,164 @@
 require 'rails_helper'
+require 'swagger_helper'
 
-RSpec.describe "Users::Registrations", type: :request do
-  describe 'POST /users' do
-    let!(:ip_info) { create(:ip_info) }
-    let(:user) { build(:user) }
+RSpec.describe 'Users::Registrations', type: :request do
+  path '/users' do
+    post 'new user registration' do
+      tags 'Users'
+      consumes 'application/json'
+      produces 'application/json'
 
-    scenario 'with unsuccessful params' do
-      post user_registration_path,
-        params: {
-          format: 'json',
+      ip_info = IpInfo.last || FactoryBot.create(:ip_info)
+      new_user = FactoryBot.build(:user, registration_ip_info: ip_info)
+      time_format = '%Y.%m.%d %H:%M:%S'
+
+      parameter name: :user, in: :body, schema: { 
+        type: :object,
+        properties: {
+          user: {
+            type: :object,
+            properties: {
+              email: { 
+                type: :string,
+                default: new_user.email
+              },
+              username: { 
+                type: :string,
+                default: new_user.username
+              },
+              password: { 
+                type: :string,
+                default: new_user.password
+              },
+              password_confirmation: { 
+                type: :string,
+                default: new_user.password_confirmation
+              },
+              registration_ip_info_attributes: {
+                type: :object,
+                properties: {
+                  address: { 
+                    type: :string,
+                    default: ip_info.address
+                  }
+                }
+              }
+            },
+            required: [
+              :username,
+              :email,
+              :password,
+              :password_confirmation,
+              :registration_ip_info_attributes
+            ]
+          }
+        },
+        required: [:user]
+      }
+
+      response '201', 'user created' do
+        let(:user) do
+          {
             user: {
-              email: '',
-              username: user.username,
-              password: user.password,
-              password_confirmation: user.password_confirmation,
+              username: new_user.username,
+              email: new_user.email,
+              password: new_user.password,
+              password_confirmation: new_user.password_confirmation,
               registration_ip_info_attributes: {
                 address: ip_info.address
               }
+            }
+          }
+        end
+
+        before do
+          User.find_by(username: new_user.username).delete if RAILS_ENV = 'test'
+        end
+
+        example 'application/json', 'success response', {
+          data: {
+            id: 1,
+            username: new_user.username,
+            email: new_user.email,
+            is_admin: new_user.is_admin,
+            registration_ip_info_id: ip_info.id,
+            created_at: "2024-04-07T17:13:30.981Z",
+            updated_at: "2024-04-07T17:13:30.981Z"
           }
         }
 
-      expect(response).to have_http_status(422)
+        run_test!
+      end
 
-      expect(
-        JSON.parse(response.body)
-      ).to include(
-        "errors" => { "email" => ["can't be blank"] }
-      )
-    end
-
-    scenario 'with successful params' do
-      post user_registration_path,
-        params: {
-          format: 'json',
-            user: {
-              email: user.email,
-              username: user.username,
-              password: user.password,
-              password_confirmation: user.password_confirmation,
-              registration_ip_info_attributes: {
-                address: ip_info.address
-              }
+      response '422', 'unprocessable content' do
+        let(:user) do
+          {
+            user: {}
           }
+        end
+
+        example 'application/json', 'missing email', {
+          errors: [
+            {
+              email: [
+                "can't be blank"
+              ]
+            }
+          ]
         }
 
-      expect(response).to have_http_status(201)
+        example 'application/json', 'missing username', {
+          errors: [
+            {
+              username: [
+                "can't be blank"
+              ]
+            }
+          ]
+        }
 
-      expect(
-        JSON.parse(response.body)['data']
-      ).to eq(User.last.as_json)
+        example 'application/json', 'missing password', {
+          errors: [
+            {
+              password: [
+                "can't be blank"
+              ]
+            }
+          ]
+        }
 
-      expect(User.last.username).to eq(user.username)
-      expect(User.last.registration_ip_info).to eq(ip_info)
+        example 'application/json', 'missing IP address', {
+          errors: [
+            {
+              "registration_ip_info.address": [
+                "can't be blank"
+              ]
+            }
+          ]
+        }
+
+        example 'application/json', 'duplicate email', {
+          errors: [
+            {
+              email: [
+                "has already been taken"
+              ]
+            }
+          ]
+        }
+
+        example 'application/json', 'duplicate username', {
+          errors: [
+            {
+              username: [
+                "has already been taken"
+              ]
+            }
+          ]
+        }
+
+        run_test!
+      end
     end
   end
 end
--- spec/requests/users/sessions_spec.rb
+++ spec/requests/users/sessions_spec.rb
@@ -1,48 +1,130 @@
 require 'rails_helper'
+require 'swagger_helper'
 
-RSpec.describe "Users::Sessions", type: :request do
-  describe 'GET /users/auth' do
-    scenario 'the user is authenticated' do
-      get users_auth_path
+RSpec.describe 'Users::Sessions', type: :request do
+  path '/users/auth' do
+    get 'authenticate user' do
+      tags 'Users'
+      consumes 'application/json'
+      produces 'application/json'
 
-      expect(response).to have_http_status(200)
+      ip_info = IpInfo.last || FactoryBot.create(:ip_info)
+      user = User.last ||
+        FactoryBot.create(:user, registration_ip_info: ip_info)
 
-      expect(
-        JSON.parse(response.body)
-      ).to include("data" => [
-        {
-          "id" => page1.id,
-          "name" => page1.name,
-         "url" => page1.url
-        },
-        {
-          "id" => page2.id,
-          "name" => page2.name,
-          "url" => page2.url
+      response '200', 'user authenticated' do
+        before { sign_in user }
+
+        example 'application/json', 'success response', {
+          data: {
+            id: 1,
+            current_user: user.username,
+            is_admin: user.is_admin
+          }
         }
-      ])
+
+        run_test!
+      end
+
+      response '401', 'unauthorised' do
+        example 'application/json', 'failure response', {
+          errors: [
+            "unauthorised"
+          ]
+        }
+
+        run_test!
+      end
     end
+  end
 
-    scenario 'the user is not authenticated' do
-      get users_auth_path
+  path '/users/sign_in' do
+    post 'sign in user' do
+      tags 'Users'
+      consumes 'application/json'
+      produces 'application/json'
 
-      expect(response).to have_http_status(401)
+      ip_info = IpInfo.last || FactoryBot.create(:ip_info)
+      user1 = User.create(FactoryBot.attributes_for(:user, registration_ip_info: ip_info))
 
-      expect(
-        JSON.parse(response.body)
-      ).to include("data" => [
-        {
-          "id" => page1.id,
-          "name" => page1.name,
-         "url" => page1.url
-        },
-        {
-          "id" => page2.id,
-          "name" => page2.name,
-          "url" => page2.url
+      parameter name: :body, in: :body, schema: { 
+        type: :object,
+        properties: {
+          user: {
+            type: :object,
+            properties: {
+              email: { 
+                type: :string,
+                default: user1.email
+              },
+              password: { 
+                type: :string,
+                default: 'Password1234!'
+              },
+            },
+            required: [
+              :email,
+              :password
+            ]
+          }
         }
-      ])
+      }, required: true
+
+      response '201', 'user signed in' do
+        before { user1 }
+
+        let(:body) do
+          {
+            user: {
+              email: user1.email,
+              password: 'Password1234!'
+            }
+          }
+        end
+
+        example 'application/json', 'success response', {
+          data: "successfully logged in"
+        }
+
+        run_test!
+      end
+
+      response '401', 'unauthorised' do
+        let(:user) do
+          {
+            user: {
+              email: user1.email,
+              password: 'wrongPassword123'
+            }
+          }
+        end
+
+        example 'application/json', 'failure response', {
+          errors: [
+            "failed authentication"
+          ]
+        }
+
+        run_test!
+      end
+    end
+  end
+
+  path '/users/sign_out' do
+    delete 'sign out user' do
+      tags 'Users'
+      consumes 'application/json'
+      produces 'application/json'
+
+      user = User.last || FactoryBot.create(:user)
+
+      response '204', 'user signed out' do
+        before { sign_in user }
+
+        example 'application/json', 'success response', { }
+
+        run_test!
+      end
     end
   end
 end
-
